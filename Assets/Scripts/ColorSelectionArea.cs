9db905e6554d1386196ebf2dc68ec0701214c291
mob programmers <mob.stagenine@gmail.com>
2016-12-14 19:40:57 -0800

    Increased area for color selection and implemented dragging

---
+++ Assets/Scripts/ColorSelectionArea.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using System;
+
+public class ColorSelectionArea : MonoBehaviour, IPointerDownHandler
+{
+
+    //enums
+
+    //subclasses
+
+    //consts and static data
+
+    //public data
+
+    //private data
+    private bool isDragging = false;
+
+    //public properties
+
+    //methods
+    #region public methods
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        isDragging = true;
+    }
+    #endregion
+
+    #region private methods
+
+    #endregion
+
+    #region monobehaviors
+    public void Update()
+    {
+        if (isDragging)
+        {
+
+#if UNITY_STANDALONE
+            if (Input.GetMouseButton(0))
+            {
+                ColorPickerPopup.singleton.PressedColorPicker();
+            }
+            else
+            {
+                isDragging = false;
+            }
+#endif
+#if UNITY_ANDROID || UNITY_IOS
+            var touches = Input.touches;
+            if(touches.Length != 0)
+            {
+                ColorPickerPopup.singleton.PressedColorPicker();
+            }
+            else
+            {
+                isDragging = false;
+            }
+#endif
+        }
+    }
+    #endregion
+
+}
